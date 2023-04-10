08fcd01db495ad4487620824111ab1db1764154c
Kender ROMAIN <kenderromain@gmail.com>
2023-04-10 15:56:26 -0400

    Create private methode of_age?.

--- person.rb
+++ person.rb
@@ -1,14 +1,21 @@
 class Person
-  def initialize (id,name="Unknown",age,parent_permission=true)
+  # Getters
+  attr_accessor :name, :age
+  # Setters
+  attr_reader :id
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
-    @age= age
+    @age = age
     @parent_permission = parent_permission
   end
 
-  # Getters
-  attr_reader :id, :name, :age
-  # Setters
-  attr_writer :name, :age
-  
+  private
+
+  def of_age?
+    return false if @age < 18
+
+    true
+  end
 end
