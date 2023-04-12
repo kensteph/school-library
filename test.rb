2961dd418ec91012137dd863a12455e50167ef3d
Kender ROMAIN <kenderromain@gmail.com>
2023-04-12 13:25:15 -0400

    Create the belongs-to relationship bettween Classroom and Student.

---
+++ test.rb
@@ -0,0 +1,14 @@
+require_relative './student.rb'
+require_relative './classroom.rb'
+
+# Create Classrooms
+classroom1 = Classroom.new("Class 1")
+classroom2 = Classroom.new("Class 2")
+classroom2 = Classroom.new("Class 3")
+
+puts classroom1.inspect
+# Create Students
+student1 = Student.new(12,classroom1,"Theddy Romain")
+student2 = Student.new(14,classroom1,"Stephy Romain")
+# puts student1.inspect
+puts classroom1.inspect
