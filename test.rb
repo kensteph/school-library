bd8e2935f8117c581b9e9ba965d687cd304b9bb0
Kender ROMAIN <kenderromain@gmail.com>
2023-04-12 13:35:11 -0400

    Make sure that when adding a student to a classroom it also sets the classroom for the student. And Make sure that when setting the classroom for a student it also adds it to the classrooms' students.

--- test.rb
+++ test.rb
@@ -4,11 +4,14 @@ require_relative './classroom.rb'
 # Create Classrooms
 classroom1 = Classroom.new("Class 1")
 classroom2 = Classroom.new("Class 2")
-classroom2 = Classroom.new("Class 3")
+classroom3 = Classroom.new("Class 3")
 
-puts classroom1.inspect
+# puts classroom1.inspect
 # Create Students
 student1 = Student.new(12,classroom1,"Theddy Romain")
 student2 = Student.new(14,classroom1,"Stephy Romain")
-# puts student1.inspect
-puts classroom1.inspect
+puts student1.inspect
+# Set a new class for student 1
+student1.classroom = classroom3
+puts student1.inspect
+# puts classroom1.inspect
