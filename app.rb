651faf36c6d2c63df33074357f8372ddb589506a
Kender ROMAIN <kenderromain@gmail.com>
2023-04-14 14:40:11 -0400

    Make all the changes required by the previous reviewer.

--- app.rb
+++ app.rb
@@ -3,6 +3,8 @@ require_relative './student'
 require_relative './book'
 require_relative './rental'
 
+
+
 class App
   def initialize
     @books_list = []
@@ -37,7 +39,7 @@ class App
   def list_all_rental()
     puts "\n"
     if @rentals_list.empty?
-      puts 'The list is empty.'
+      puts 'The rental list is empty.'
     else
       print 'ID of the person : '
       person_id = gets.chomp.to_i
@@ -68,7 +70,7 @@ class App
   def create_student
     puts "\n"
     puts 'Please provide these information :'
-    print 'Classroom (number) :'
+    print 'Classroom (number) : '
     classroom = gets.chomp
     print 'Age : '
     age = gets.chomp
@@ -128,40 +130,12 @@ class App
     puts "\n"
   end
 
-  def display_menu
-    puts 'Please choose an option by entering a number:'
-    puts '1 - List all book'
-    puts '2 - List all people'
-    puts '3 - Create a person'
-    puts '4 - Create a book'
-    puts '5 - Create a rental'
-    puts '6 - List all rentals for a given person id'
-    puts '7 - Exit'
-  end
-
-  # rubocop:disable Metrics/CyclomaticComplexity
-  def menu()
-    choice = ''
-    while choice != 7.to_s
-      display_menu
-      choice = gets.chomp
-      case choice
-      when '1' then list_all_books
-      when '2' then list_all_people
-      when '3' then create_person
-      when '4' then create_book
-      when '5' then create_rental
-      when '6'then list_all_rental
-      when '7'then exit_app
-      else
-        puts "Please select a number between 1 and 7\n\n"
-      end
+  def actions_management(choice)
+    action_to_perform = ACTIONS[choice]
+    if action_to_perform.nil?
+      puts "\nYour choice is incorrect. Must between 1-7. Please retry...\n\n"
+    else
+      send(action_to_perform)
     end
-  end
-  # rubocop:enable Metrics/CyclomaticComplexity
-
-  def exit_app
-    puts 'Thank you for using this app!'
-    exit
   end
 end
