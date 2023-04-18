18d53b1cf18ac2209a9c7fda8e4b37d923c6e8ab
Kender ROMAIN <kenderromain@gmail.com>
2023-04-17 23:57:44 -0400

    Fix linter errors.

--- database.rb
+++ database.rb
@@ -37,11 +37,11 @@ module Database
       book = { title: rental.book.title, author: rental.book.author }
       if rental.person.instance_of?(::Student)
         student = { age: rental.person.age, classroom: rental.person.classroom, name: rental.person.name,
-                    id: rental.person.id,type: 'Student' }
+                    id: rental.person.id, type: 'Student' }
         rental_ojects << { date: rental.date, book: book, person: student }
       else
         teacher = { age: rental.person.age, specialization: rental.person.specialization, name: rental.person.name,
-                    id: rental.person.id,type: 'Teacher' }
+                    id: rental.person.id, type: 'Teacher' }
         rental_ojects << { date: rental.date, book: book, person: teacher }
       end
     end
@@ -62,14 +62,14 @@ module Database
     if File.exist?('people.json') && !File.empty?('people.json')
       data = JSON.parse(File.read('people.json'))
       data.each do |person|
-        if person['type'] == "Student"
-          student = Student.new(person['age'],person['classrom'],person['name'] )
+        if person['type'] == 'Student'
+          student = Student.new(person['age'], person['classrom'], person['name'])
           student.id = person['id']
-          people<< student
+          people << student
         else
-          teacher = Teacher.new(person['age'],person['specialization'],person['name'] )
+          teacher = Teacher.new(person['age'], person['specialization'], person['name'])
           teacher.id = person['id']
-          people<< teacher
+          people << teacher
         end
       end
     end
@@ -82,20 +82,17 @@ module Database
       data = JSON.parse(File.read('rentals.json'))
       data.each do |rental|
         book = Book.new(rental['book']['title'], rental['book']['author'])
-        if rental['person']['type'] == "Student"
-          student = Student.new(rental['person']['age'],rental['person']['classrom'],rental['person']['name'] )
+        if rental['person']['type'] == 'Student'
+          student = Student.new(rental['person']['age'], rental['person']['classrom'], rental['person']['name'])
           student.id = rental['person']['id']
-          rentals<< Rental.new(rental['date'],book,student) 
+          rentals << Rental.new(rental['date'], book, student)
         else
-          teacher = Teacher.new(rental['person']['age'],rental['person']['specialization'],rental['person']['name'] )
+          teacher = Teacher.new(rental['person']['age'], rental['person']['specialization'], rental['person']['name'])
           teacher.id = rental['person']['id']
-          rentals<< Rental.new(rental['date'],book,teacher)
+          rentals << Rental.new(rental['date'], book, teacher)
         end
       end
     end
     rentals
   end
-
-
-
 end
