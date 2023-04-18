d1ab3f44d73eec12e4a341e8e1ac7e6c9788bf93
Kender ROMAIN <kenderromain@gmail.com>
2023-04-17 23:08:29 -0400

    Create load_books method.

--- database.rb
+++ database.rb
@@ -5,6 +5,10 @@ module Database
     puts "All the data has been preserved successfully.\n"
     # Preserve Books
     preseve_books
+    # Preserve People
+    preseve_people
+    # Preserve Rentals
+    preseve_rentals
   end
 
   def preseve_books
@@ -17,16 +21,35 @@ module Database
     people_ojects =[]
     @people_list.each do |people| 
       if people.class.name == 'Student'
-        people_ojects << {age: people.age,classroom:people.classroom,name:people.name,parent_permission:people.parent_permission,type:"Student"}
+        people_ojects << {age: people.age,classroom:people.classroom,name:people.name,parent_permission:people.parent_permission,id: people.id,type:"Student"}
       else
-        people_ojects << {age: people.age,specialization:people.specialization,name:people.name,type:"Teacher"}
+        people_ojects << {age: people.age,specialization:people.specialization,name:people.name,id: people.id,type:"Teacher"}
       end
       
     end
     File.write("people.json",people_ojects.to_json)
   end
 
-  def query_books
+  def preseve_rentals
+    rental_ojects =[]
+
+    @rentals_list.each do |rental| 
+      
+      if rental.person.class.name == 'Student'
+        student = {age: rental.person.age,classroom:rental.person.classroom,name:rental.person.name,id: rental.person.id}
+        book = {title: rental.book.title,author: rental.book.author}
+        rental_ojects << {date: rental.date,book: book,person: student}
+      else
+        teacher = {age: rental.person.age,specialization:rental.person.specialization,name:rental.person.name,id: rental.person.id}
+        book = {title: rental.book.title,author: rental.book.author}
+        rental_ojects << {date: rental.date,book: book,person: teacher}
+      end
+      
+    end
+    File.write("rentals.json",rental_ojects.to_json)
+  end
+
+  def load_books
     books = []
     if File.exist?("books.json") && File.read("books.json") !='' 
       data = JSON.parse(File.read("books.json")) 
